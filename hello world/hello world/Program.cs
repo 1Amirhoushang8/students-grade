de92fd1a9929748065faf6e96b6c4cd0fd4aaff1
1Amirhoushang8 <houshamir@gmail.com>
2025-03-20 11:22:18 +0330

    Add files via upload
    open program.cs to see the code

---
+++ hello world/hello world/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("how many students you have? ");
+
+int students = int.Parse(Console.ReadLine());
+
+int counter = 1;
+
+int sum = 0;
+
+while (counter <= students)
+{
+    Console.WriteLine("please enter the student grade: ");
+    
+    int grade = int.Parse(Console.ReadLine());
+
+    sum = sum + grade;
+
+    counter++;
+
+}
+
+double average = (double)sum / students;
+
+Console.WriteLine("the average grade of your students is : " + " " + average);
